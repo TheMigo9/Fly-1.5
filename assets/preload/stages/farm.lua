60b18b07af6f66f4d9a6c23b44ad9a455555ce4c
TheMigo9 <162376189+TheMigo9@users.noreply.github.com>
2024-04-05 22:02:15 +0200

    Add files via upload

---
+++ assets/preload/stages/farm.lua
@@ -0,0 +1,18 @@
+function onCreate()
+
+    makeLuaSprite('theDark','farm/farm',-610,-300)
+	addLuaSprite('theDark',false)
+	close(true)
+end
+
+function onBeatHit( ... )--for every beat
+	-- body
+end
+
+function onStepHit( ... )--for every step
+	-- body
+end
+
+function onUpdate( ... )
+	-- body
+end
---
+++ assets/preload/stages/stage2.lua
@@ -0,0 +1,7 @@
+function onCreate()
+	-- background shit
+	makeLuaSprite('void', '', -300, -300)
+	makeGraphic('void', 1920 * 1.5, 1080 * 1.5, 'FFFFFF')
+
+	addLuaSprite('void', false)
+end
